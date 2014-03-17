9cec37f86e43492a22fa9caee2f6b22898d7b3d4
John Ryding <john.ryding@inin.com>
2014-03-16 22:40:48 -0400

    Create has_many relationship between the users and plugins models

--- app/models/plugin.rb
+++ app/models/plugin.rb
@@ -1,2 +1,7 @@
 class Plugin < ActiveRecord::Base
+    belongs_to :user
+
+    validates :title, :homepage, presence: true
+    validates :description, length: { in: 5..500 }
+    validates :homepage, :format => URI::regexp(%w(http https))
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :plugins
 end
